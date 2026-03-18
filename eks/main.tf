591a0ef8ec0850d542d761f05a2146c1ddff1833
Sai Kiran <sai.buildxact1@gmail.com>
2026-03-19 07:25:16 +1100

    Adding Eks Module

---
+++ eks/main.tf
@@ -0,0 +1,8 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+}
---
+++ eks/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_role_arn" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
